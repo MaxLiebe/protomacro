b7aab887451ecc9b4073170874c1e8234197f990
MaxLiebe <amalox@outlook.com>
2023-01-24 12:05:33 +0100

    Initial commit

---
+++ config.h
@@ -0,0 +1,8 @@
+// explanation:
+// https://github.com/qmk/qmk_firmware/blob/master/docs/config_options.md
+
+#pragma once
+
+#define ENCODERS_PAD_A {B3, B3}
+#define ENCODERS_PAD_B {B1, B2}
+#define ENCODER_DIRECTION_FLIP
