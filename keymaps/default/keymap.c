b7acb957591fa8770904d388e412e06396350e25
Max Liebe <amalox@outlook.com>
2023-01-30 16:54:43 +0100

    prepped for unicode

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,5 +1,15 @@
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    PIZZA,
+    MOON
+};
+
+const uint32_t unicode_map[] PROGMEM = {
+    [PIZZA]  = 0x1F355,  // ‽
+    [MOON] = 0x1F315
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┐
@@ -11,17 +21,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x3(
-        KC_AUDIO_VOL_UP,    KC_AUDIO_VOL_DOWN,    KC_MEDIA_PLAY_PAUSE	,
+        KC_A,    KC_B,    KC_C,
         KC_D,    KC_E,    KC_F,
         KC_G,    KC_H,    KC_I
     )
 };
 
-#if defined(ENCODER_MAP_ENABLE)
+//#if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { 
         ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), 
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  
     }
 };
-#endif
+//#endif
