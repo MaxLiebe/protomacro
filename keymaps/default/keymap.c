102118b1a7a9022633fc666b867ebd9dd29521c1
Mart1nus <136019martijn@gmail.com>
2023-02-10 10:36:52 +0100

    everything is button

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -6,7 +6,7 @@ enum unicode_names {
 };
 
 const uint32_t unicode_map[] PROGMEM = {
-    [PIZZA]  = 0x1F355,  // ‽
+    [PIZZA]  = 0x1F355, 
     [MOON] = 0x1F315
 };
 
@@ -19,19 +19,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┤
      * │ G │ H │ I │
      * └───┴───┴───┘
+     * │ M │   │ N │
+     * └───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x3(
         KC_A,    KC_B,    KC_C,
         KC_D,    KC_E,    KC_F,
-        KC_G,    KC_H,    KC_I
+        KC_G,    KC_H,    KC_I,
+        KC_N,    KC_O
     )
 };
 
 //#if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { 
-        ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), 
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  
+        ENCODER_CCW_CW(KC_J, KC_K), 
+        ENCODER_CCW_CW(KC_L, KC_M)  
     }
 };
 //#endif
