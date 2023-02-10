d958efa3cbbb13db5e49952da11e8ad36a5f8a87
Max Liebe <amalox@outlook.com>
2023-02-10 12:22:26 +0100

    Fixed emoji support on Windows (requires WinCompose)

--- config.h
+++ config.h
@@ -6,3 +6,7 @@
 #define ENCODERS_PAD_A {F4, F4}
 #define ENCODERS_PAD_B {F5, F6}
 #define ENCODER_DIRECTION_FLIP
+
+// uncomment this line if you are on Windows and want to use emojis
+// make sure you have WinCompose installed
+// #define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x3(
-        KC_A,    KC_B,    KC_C,
+        X(PIZZA),    KC_B,    KC_C,
         KC_D,    KC_E,    KC_F,
         KC_G,    KC_H,    KC_I,
         KC_N,    KC_O
